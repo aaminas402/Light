0b5ffab6e148b1c01c6794a0442db6f0dda03165
Aamina S <117933292+aaminas402@users.noreply.github.com>
2024-07-29 11:25:33 +0530

    Create cartoon.py

---
+++ cartoon.py
@@ -0,0 +1,29 @@
+import json
+from generate_panels import generate_panels
+from stability_ai import text_to_image
+from add_text import add_text_to_panel
+from create_strip import create_strip
+
+STYLE = "american comic, colored"
+
+def main(scenario):
+    panels = generate_panels(scenario)
+
+    with open('output/panels.json', 'w') as outfile:
+        json.dump(panels, outfile)
+
+    panel_images = []
+
+    for panel in panels:
+        panel_prompt = panel["description"] + ", cartoon box, " + STYLE
+        print(f"Generate panel {panel['number']} with prompt: {panel_prompt}")
+        panel_image = text_to_image(panel_prompt)
+        panel_image_with_text = add_text_to_panel(panel["text"], panel_image)
+        panel_image_with_text.save(f"output/panel-{panel['number']}.png")
+        panel_images.append(panel_image_with_text)
+
+    create_strip(panel_images).save("output/strip.png")
+
+if __name__ == "__main__":
+    scenario = input("Enter the scenario: ")
+    main(scenario)
